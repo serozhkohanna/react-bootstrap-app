a32af88bafbedcde5f28f0094039b5b556ab2391
ext.vryabchich <ext.vryabchich@tinkoff.ru>
2019-05-07 23:33:24 +0300

    Добавлен base ревест

--- src/services/getPoints.js
+++ src/services/getPoints.js
@@ -1,12 +1,15 @@
 import setPoints from '../actions/setPoints';
+import base from './base';
 
 import prop from '@tinkoff/utils/object/prop';
 
 import request from 'superagent';
 
 export default function getPoints () {
-    return dispatch => request
-        .get('/api/example/get-points')
+    return dispatch => base(
+        request
+            .get('/api/example/get-points')
+    )
         .then(payload => {
             const points = prop('body', payload);
 
