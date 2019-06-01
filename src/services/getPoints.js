9811f619f40feed448457b58670879a8f3acedf9
Belous <v.bilous@andersenlab.com>
2019-06-01 18:17:03 +0300

    Поправлен запрос за поинтами

--- src/services/getPoints.js
+++ src/services/getPoints.js
@@ -1,8 +1,6 @@
 import setPoints from '../actions/setPoints';
 import base from './base';
 
-import prop from '@tinkoff/utils/object/prop';
-
 import request from 'superagent';
 
 export default function getPoints () {
@@ -11,8 +9,6 @@ export default function getPoints () {
             .get('/api/example/get-points')
     )
         .then(payload => {
-            const points = prop('body', payload);
-
-            return dispatch(setPoints(points));
+            return dispatch(setPoints(payload));
         });
 }
